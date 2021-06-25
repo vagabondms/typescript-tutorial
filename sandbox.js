8d7e9938cdf47677bef7d10e463dc8a99d2ce7a2
vagabondms <hiseokseok@gmail.com>
2021-06-25 23:18:35 +0900

    :sparkles: lesso-3 완료

--- sandbox.js
+++ sandbox.js
@@ -1,14 +1,10 @@
 var character = 'mario';
 var age = 30;
 var isBlackBelt = false;
-// character = 20;
 character = 'luigi';
-// age = 'yoshi';
 age = 40;
-// isBlackBelt = 'yes';
 isBlackBelt = true;
-var area = function (diameter) {
+var circ = function (diameter) {
     return diameter * Math.PI;
 };
-// console.log(area('hello'));
-console.log(area(7.5));
+console.log(circ(1));
