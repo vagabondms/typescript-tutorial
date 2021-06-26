a16638d19eaba4f41b2ad7e530a6b56a8ea3522d
vagabondms <hiseokseok@gmail.com>
2021-06-26 13:41:43 +0900

    :sparkles: lesson-5

--- sandbox.js
+++ sandbox.js
@@ -1,3 +1,35 @@
-// arrays 
-var names = ['luigi,', mario, ',', yoshi, ']];
-// objects 
+// explicit types
+var character;
+var age;
+var isLoggedIn;
+// age = 'luigi'
+age = 30;
+//isLoggedIN = 25;
+isLoggedIn = true;
+// arrays
+var ninjas = [];
+// ninjas = [10,23] //! 안된다.
+// ninjas = ['yoshi', 'mario']; //! 된다.
+ninjas.push('shaun');
+// union types
+var mixed = [];
+mixed.push('hello');
+mixed.push(20);
+// mixed.push(false) //!  불가!
+// let mixed: (string | number | boolean)[] = [];
+console.log(mixed);
+var uid = 'hi';
+//objects
+// * 아래도 가능하긴 하다. 하지만 권장하지 않음
+var ninjaOne;
+ninjaOne = {
+    name: 'yoshi',
+    age: 30
+};
+//! 사전에 설정한 것이다.
+var ninjaTwo;
+ninjaTwo = {
+    name: 'mario',
+    age: 20,
+    beltColor: 'black'
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,20 +1,49 @@
+// explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+// age = 'luigi'
+age = 30;
+
+//isLoggedIN = 25;
+isLoggedIn = true;
+
 // arrays
-let names = ['luigi', 'mario', 'yoshi'];
+let ninjas: string[] = [];
+// ninjas = [10,23] //! 안된다.
+// ninjas = ['yoshi', 'mario']; //! 된다.
 
-names.push('toad');
-// names.push(3);
-// names[0] = 3;
+ninjas.push('shaun');
 
-let numbers = [10, 20, 30, 40];
-numbers.push(25);
-// numbers.push('minseok');
-// numbers[1] = 'minseok';
+// union types
+let mixed: (string | number)[] = [];
+mixed.push('hello');
+mixed.push(20);
+// mixed.push(false) //!  불가!
+// let mixed: (string | number | boolean)[] = [];
+console.log(mixed);
 
-let mixed = ['key', 4, 'hi', 6, 4];
+let uid: string | number = 'hi';
 
-// objects
-let ninja = {
-  name: 'mario',
-  belt: 'black',
+//objects
+// * 아래도 가능하긴 하다. 하지만 권장하지 않음
+let ninjaOne: object;
+ninjaOne = {
+  name: 'yoshi',
   age: 30,
+};
+
+//! 사전에 설정한 것이다.
+let ninjaTwo: {
+  name: string;
+  age: number;
+  beltColor: string;
+};
+
+ninjaTwo = {
+  name: 'mario',
+  age: 20,
+  beltColor: 'black',
+  //   skills: [], //! 이부분은 실행되지 않음
 };
