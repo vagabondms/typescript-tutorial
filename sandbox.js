39b3df5444f840f30073102260766051c94a03a8
vagabondms <hiseokseok@gmail.com>
2021-06-26 16:12:37 +0900

    :sparkles: lesson-9

--- sandbox.js
+++ sandbox.js
@@ -1,23 +1,6 @@
-/* any type */
-// 아래의 모든 것들이 잘 동작한다.
-var age = 25;
-age = true;
-console.log(age);
-age = 'hello';
-console.log(age);
-age = { name: 'luigi' };
-console.log(age);
-// 아래의 모든 것들이 다 동작한다.
-var mixed = [];
-mixed.push(5);
-mixed.push('mario');
-mixed.push(false);
-console.log(mixed);
-var ninja = {
-    name: 'hi',
-    age: 'hi'
+var logDetails = function (uid) {
+    console.log(uid);
 };
-ninja = {
-    name: 25,
-    age: 'yoshi'
+var greet = function (user) {
+    console.log(user.name + " says hello\n  ");
 };
--- sandbox.ts
+++ sandbox.ts
@@ -1,28 +1,11 @@
-/* any type */
+type StringOrNum = string | number;
+type objWithName = { name: string; uid: StringOrNum };
 
-// 아래의 모든 것들이 잘 동작한다.
-let age: any = 25;
-age = true;
-console.log(age);
-age = 'hello';
-console.log(age);
-age = { name: 'luigi' };
-console.log(age);
-
-// 아래의 모든 것들이 다 동작한다.
-let mixed: any[] = [];
-
-mixed.push(5);
-mixed.push('mario');
-mixed.push(false);
-console.log(mixed);
-
-let ninja: { name: any; age: any } = {
-  name: 'hi',
-  age: 'hi',
+const logDetails = (uid: StringOrNum) => {
+  console.log(uid);
 };
 
-ninja = {
-  name: 25,
-  age: 'yoshi',
+const greet = (user: objWithName) => {
+  console.log(`${user.name} says hello
+  `);
 };
