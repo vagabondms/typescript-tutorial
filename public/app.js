a0953ad4d1c662b51a4204124221bb71d11501b8
vagabondms <hiseokseok@gmail.com>
2021-06-26 17:57:42 +0900

    :sparkles: lesson-13

--- public/app.js
+++ public/app.js
@@ -1,10 +1,19 @@
 "use strict";
 // classes
 var Invoice = /** @class */ (function () {
-    function Invoice(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // public client: string; // 읽고 쓰기 가능
+    // readonly details: string; // 읽기만 가능
+    // private amount: number; // class 안에서만 읽기가 가능.
+    // constructor(c: string, d: string, a: number) {
+    //   this.client = c;
+    //   this.details = d;
+    //   this.amount = a;
+    // }
+    // ! 위를 단축해서 아래와 같이 사용할 수 있다.
+    function Invoice(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     Invoice.prototype.format = function () {
         return this.client + " owes $" + this.amount + " for " + this.details;
@@ -18,8 +27,21 @@ var invoices = [];
 //! invoices.push({name: minseok}) not allowed
 invoices.push(invOne); // allowed
 invoices.push(invTwo); // allowed
-invOne.client = 'yoshi'; //allowed
-//invTwo.amount = 'hi' //! not allowed
+/*
+  invOne.client = 'yoshi'; //allowed
+  invTwo.amount = 'hi' //! not allowed
+  이러한 동작을 막아야 한다.
+*/
+invoices.forEach(function (inv) {
+    console.log(inv.client, inv.details, inv.format());
+    // * inv.client = 'hi'; 이런 동작도 가능하다. public으로 설정 되어있기 때문임.
+});
+/*
+  Invoice 클래스는 접근이 가능하다. 개별 property들이 접근하거나 변경할 수 있다.
+*/
+/*
+
+*/
 var form = document.querySelector('.new-item-form');
 // console.log(form.children);
 //!  inputs
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,19 @@
 // classes
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
-  }
+  // public client: string; // 읽고 쓰기 가능
+  // readonly details: string; // 읽기만 가능
+  // private amount: number; // class 안에서만 읽기가 가능.
+  // constructor(c: string, d: string, a: number) {
+  //   this.client = c;
+  //   this.details = d;
+  //   this.amount = a;
+  // }
+  // ! 위를 단축해서 아래와 같이 사용할 수 있다.
+  constructor(
+    public client: string,
+    readonly details: string,
+    private amount: number
+  ) {}
 
   format() {
     return `${this.client} owes $${this.amount} for ${this.details}`;
@@ -23,8 +29,24 @@ let invoices: Invoice[] = [];
 invoices.push(invOne); // allowed
 invoices.push(invTwo); // allowed
 
-invOne.client = 'yoshi'; //allowed
-//invTwo.amount = 'hi' //! not allowed
+/* 
+  invOne.client = 'yoshi'; //allowed
+  invTwo.amount = 'hi' //! not allowed
+  이러한 동작을 막아야 한다. 
+*/
+
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.details, inv.format());
+  // * inv.client = 'hi'; 이런 동작도 가능하다. public으로 설정 되어있기 때문임.
+});
+
+/* 
+  Invoice 클래스는 접근이 가능하다. 개별 property들이 접근하거나 변경할 수 있다.
+*/
+
+/*
+
+*/
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 // console.log(form.children);
